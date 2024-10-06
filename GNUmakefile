263162c02f1ffdbff396d5baa02cfcc96f615ab6
barnabasp <barnabas.porfy@gmail.com>
2024-10-06 20:59:21 +0200

    fix after merge

--- GNUmakefile
+++ GNUmakefile
@@ -3,11 +3,7 @@
 # GNUmakefile for examples module.  Gabriele Cosmo, 06/04/98.
 # --------------------------------------------------------------
 
-<<<<<<< HEAD
-name := exampleB1
-=======
 name := mieNorma
->>>>>>> macro
 G4TARGET := $(name)
 G4EXLIB := true
 
--- runwithtimelog.sh
+++ runwithtimelog.sh
@@ -1,12 +1,4 @@
 cd build
-<<<<<<< HEAD
-time ./exampleB1 run2.mac -t 1 &> example0.log
-#time ./exampleB1 run2.mac -t 2> mie.err 1> mie.out
-cd ..
-#grep "mag,phi,theta:" build/mie.out | awk '{print $(NF-1), $NF}' > build/mie_filter.out
-root.exe -b -q 'make_histogram.C("build/output100M.root", 100, "#theta", "pngname")'
-#root.exe -b -q 'make_histogram_old.C("build/mie_filter.out", 100, "#theta", "thetadist.png")'
-=======
 time ./mieNorma run2.mac -t 1 &> mieNorma.log
 #time ./mieNorma run2.mac -t 2> mie.err 1> mie.out
 cd ..
@@ -14,4 +6,3 @@ cd ..
 root.exe -b -q 'plotCrossSection.C("build/output0.root", 100, "#theta", "thetadist.png",0.,3.14)' &> build/histprintout.log
 #root.exe -b -q 'plotCrossSection.C("build/output100M.root", 100, "#theta", "thetadist.png",0.,3.14)'
 #root.exe -b -q 'plotCrossSection_old.C("build/mie_filter.out", 100, "#theta", "thetadist.png",0.,3.14)'
->>>>>>> macro
